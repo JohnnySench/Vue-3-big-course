6381ebcc00950cb1cf905e44f32357e843f4e9b8
Jeka <jonrovniy@yandex.ru>
2023-09-30 12:14:25 +0300

    add transition for posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -18,14 +18,30 @@ export default {
   <div>
     <h3 v-if="posts.length > 0">Список постов</h3>
     <h3 class="text-red" v-else>Список постов пуст</h3>
-    <PostItem
-        @remove="this.$emit('remove', post)"
-        v-for="post in posts"
-        :post="post"
-        :key="post.id"/>
+    <transition-group name="user-list">
+      <PostItem
+          @remove="this.$emit('remove', post)"
+          v-for="post in posts"
+          :post="post"
+          :key="post.id"/>
+    </transition-group>
   </div>
 </template>
 
 <style scoped>
+.user-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
 
+.user-list-enter-active,
+.user-list-leave-active {
+  transition: all 1s ease;
+}
+
+.user-list-enter-from,
+.user-list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>
